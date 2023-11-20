c71190c1e3d81f29ae4a844f6def6f7e7751bcdf
Ethan0309 <78483249+Ethan9030@users.noreply.github.com>
2023-11-20 20:32:34 +0000

    Update breadth-first.pl

--- breadth-first.pl
+++ breadth-first.pl
@@ -1,25 +1,20 @@
+search_bf :-
+    my_agent(A),
+    get_agent_position(A,P),
+    (complete(P) -> true
+    ;otherwise   -> search_bf([[P]],[],Path),
+                    agent_do_moves(A,Path)).
+   
+search_bf(Queue,Visited,Path) :-
+    Queue = [Next|Rest],
+    Next = [Pos|RPath],
+    (complete(Pos) -> reverse([Pos|RPath],[_|Path])
+    ;otherwise     -> (findall([NP,Pos|RPath],
+                               (map_adjacent(Pos,NP,empty),\+ member(NP,Visited), \+ member([NP|_],Rest)),
+                               Newfound),
+                      append(Rest,Newfound,NewQueue),
+                      search_bf(NewQueue,[Pos|Visited],Path))).
+    
 % Test if the objective has been completed at a given position
 complete(P) :- 
     map_adjacent(P,_,o(_)).
-
-% Perform a BFS to find the nearest oracle
-search_bf :-
-    my_agent(A),  
-    get_agent_position(A,P),
-    (
-      complete(P) -> true;
-      otherwise -> search_bf([[P]],[],Path),
-                   agent_do_moves(A,Path)
-    ).
-
-search_bf(Queue,Visited,Path) :- 
-    Queue = [Current | Rest],
-    Current = [Pos | RPath],
-    (
-      complete(Pos) -> reverse([Pos | RPath],[_ | Path]);
-      otherwise -> findall(
-        [NP,Pos | RPath], map_adjacent(Pos,NP,empty), \+member(NP,Visited), \+member(NP,Rest), Newfound
-      ),
-      append(Queue, Newfound, NewQueue),
-      search_bf(NewQueue,[NP,Pos | RPath],Path)
-    ).
